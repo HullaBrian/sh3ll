c16267498edf8d6ce390677c303f0ea108c776e7
Beier <Beierlejc@gmail.com>
2022-04-11 21:04:48 -0500

    Fixed version

--- sh3ll/__init__.py
+++ sh3ll/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 
-__version__ = "1.0.2"  # Be sure to update version in setup.py as well
+__version__ = "1.0.3"  # Be sure to update version in setup.py as well
 
 from difflib import SequenceMatcher
 from sh3ll.command import command
